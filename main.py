0f9faa27d535eef39f25eb5c7d8ea11e5ebc0e0a
林天舒 <lintianshu@MacBook-Pro-621.local>
2023-04-21 18:23:02 -0400

    final project final version

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+##SI507 Final Project##
+######tianshu lin######
+######tianshul#########
+
+
 import requests
 import networkx as nx
 import matplotlib.pyplot as plt
+import json
+import os
 
 
 def search_books_by_author(author):
+    """
+    Search for books written by a given author using the Google Books API.
+
+    Parameters
+    ----------
+    param1 author : str
+        The name of the author to search for.
+
+    Returns
+    -------
+    list of dict
+        A list of dictionaries containing information about the books found. 
+        Each dictionary contains the book title and ID.
+    """
     try:
         # Set up the API endpoint URL and search query parameters
         url = 'https://www.googleapis.com/books/v1/volumes'
@@ -25,6 +46,7 @@ def search_books_by_author(author):
             title = book['volumeInfo']['title']
             book_id = book['id']
             books.append({'title': title, 'id': book_id})
+        print(f"The len of the data is available: {len(books)}")
         return books
 
     except requests.exceptions.HTTPError as HE:
@@ -35,9 +57,20 @@ def search_books_by_author(author):
         return "Please Enter the valid Author name"
 
 
-
-# Function to search for books by title
 def search_books_by_title(title):
+    """
+    Search for books with a given title using the Google Books API.
+
+    Parameters
+    ----------
+    param1 title : str
+        The title of the book to search for.
+
+    Returns
+    -------
+    str
+        A string of book titles and authors separated by newline characters.
+    """
     try:
         # Set up the API endpoint URL and search query parameters
         url = 'https://www.googleapis.com/books/v1/volumes'
@@ -67,8 +100,20 @@ def search_books_by_title(title):
         return "Please Enter the valid book title"
 
 
-# Function to get detailed information for a book by ID
 def get_book_info(book_id):
+    """
+    Retrieve information about a book with a given ID using the Google Books API.
+
+    Parameters
+    ----------
+    param1 book_id : str
+        The ID of the book to retrieve information for.
+
+    Returns
+    -------
+    str
+        A string containing the book's title, author(s), and description.
+    """
     try:
         # Set up the API endpoint URL and make the API request
         url = f'https://www.googleapis.com/books/v1/volumes/{book_id}'
@@ -88,8 +133,21 @@ def get_book_info(book_id):
         return "Please Enter the valid Book ID"
 
 
-# Function to create a network diagram of an author's co-authors
 def author_network(author):
+    """
+    Searches the Google Books API for books written by the given author and
+    extracts a list of co-authors.
+    Parameters
+    ----------
+    param1 author : str
+        The name of the author to search for.
+
+    Returns
+    -------
+    list
+        list of strings representing the names of co-authors of the given
+        author. If no co-authors are found, an empty list is returned.
+    """
     try:
         # Set up the API endpoint URL and search query parameters
         url = 'https://www.googleapis.com/books/v1/volumes'
@@ -107,23 +165,15 @@ def author_network(author):
                 co_authors += book['volumeInfo']['authors']
         co_authors = list(set(co_authors))  # Remove duplicates
 
-        # Create a network graph of the co-authors
-        G = nx.Graph()
-        G.add_node(author)
-        for co_author in co_authors:
-            G.add_node(co_author)
-            G.add_edge(author, co_author)
-
-        # Plot the network graph
-        pos = nx.spring_layout(G)
-        plt.figure(figsize=(10, 10))
-        nx.draw_networkx_nodes(G, pos, node_color='r', node_size=500, alpha=0.8)
-        nx.draw_networkx_labels(G, pos, font_size=12, font_family='sans-serif')
-        nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
-        plt.axis('off')
-        plt.show()
-        plt.savefig("Author_Co Author network.png")
-
+        filename = f"{author}_coauthors.json"
+        # Check if the file exists and delete it if it does
+        if os.path.exists(filename):
+            os.remove(filename)
+        # Write the JSON data to the file
+        with open(filename, 'w') as f:
+            json.dump(co_authors, f)
+        print(filename)
+        return co_authors
     except requests.exceptions.HTTPError as HE:
         print(f'Error occurred during API request: {HE}')
     except requests.exceptions.RequestException as RE:
@@ -133,75 +183,101 @@ def author_network(author):
     except Exception as e:
         print(f'An unexpected error occurred: {e}')
 
-user_input = input('Do you want to search by author or by book title? Please answer either author or title:')
+
+def plot_network(author, filename):
+    """
+    Retrieve information about a book with a given ID using the Google Books API.
+
+    Parameters
+    ----------
+    param1 author : str
+        The name of the author whose co-authors should be plotted.
+    param2 filename : str
+        The name of the file containing the list of co-authors (in JSON format).
+    Returns
+    -------
+    None
+    """
+    try:
+        with open(filename) as f:
+            co_authors = json.load(f)
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found")
+        return
+
+    plt.ion()
+    # Create a network graph of the co-authors
+    G = nx.Graph()
+    G.add_node(author)
+    for co_author in co_authors:
+        G.add_node(co_author)
+        G.add_edge(author, co_author)
+
+    # Plot the network graph
+    pos = nx.spring_layout(G)
+    plt.figure(figsize=(10, 10))
+    nx.draw_networkx_nodes(G, pos, node_color='r', node_size=500, alpha=0.8)
+    nx.draw_networkx_labels(G, pos, font_size=12, font_family='sans-serif')
+    nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
+    plt.axis('off')
+    plt.show()
+    plt.pause(5)
+    # Disable interactive mode and close the plot
+    plt.ioff()
+    plt.close()
+
+
 if __name__ == '__main__':
-    if user_input == 'author':
-        author = input("Enter an author's name: ")
-        books = search_books_by_author(author)
-        print(f"Books by {author}:")
-        for book in books:
-            print(f"{book['title']} ({book['id']})")
-        print()
-    if user_input == 'title':
-        title = input("Enter a book title: ")
-        print("Searching on website...")
-        results = search_books_by_title(title)
-        print(f"Search results for '{title}':")
-        print(results)
-        print() 
-    else: 
-        print("please enter only 'author' or 'title' to speific your searching")
+    while True:
+        # Ask the user which task they would like to perform
+        task = input("Select a task:\n1. Search books by author\n2. Search books by title\n3. Get book info\n4. See author's co-author network\n5. Exit\n")
+        
+        if task == '1':
+            author = input("Enter an author's name: ")
+            books = search_books_by_author(author)
+            print(f"Books by {author}:")
+            for book in books:
+                print(f"{book['title']} ({book['id']})")
+            print()
 
+            more_info = input("Do you want more information about a book? (y/n) ")
+            if more_info.lower() == 'y':
+                book_id = input("Enter a book ID: ")
+                print("Searching on website...")
+                info = get_book_info(book_id)
+                print(info)
+                print()
 
+        elif task == '2':
+            title = input("Enter a book title: ")
+            print("Searching on website...")
+            results = search_books_by_title(title)
+            print(f"Search results for '{title}':")
+            print(results)
+            print()
 
+        elif task == '3':
+            book_id = input("Enter a book ID: ")
+            print("Searching on website...")
+            info = get_book_info(book_id)
+            print(info)
+            print()
 
+        elif task == '4':
+            author = input("Enter an author's name: ")
+            create_network = input("Do you want to create a network diagram of the author's co-authors? (y/n): ")
+            if create_network.lower() == 'y':
+                co_authors = author_network(author)
+                plot_network(author, filename=f"{author}_coauthors.json")
+            else:
+                author_network(author=author)
+        elif task == '5':
+            break
+        else:
+            print("Invalid input, please try again.")
+        
+        # Ask the user if they would like to perform another task
+        again = input("Would you like to perform another task? (y/n) ")
+        if again.lower() != 'y':
+            break
 
-""" if __name__ == '__main__':
-    # Call the search_books_by_author() function and print the results
-    author = input("Enter an author's name: ")
-    books = search_books_by_author(author)
-    print(f"Books by {author}:")
-    for book in books:
-        print(f"{book['title']} ({book['id']})")
-    print()
-
-    # Call the search_books_by_title() function and print the results
-    title = input("Enter a book title: ")
-    print("Searching on website...")
-    results = search_books_by_title(title)
-    print(f"Search results for '{title}':")
-    print(results)
-    print() 
- """
-
-
-""" if __name__ == '__main__':
-    # Call the search_books_by_author() function and print the results
-    author = input("Enter an author's name: ")
-    books = search_books_by_author(author)
-    print(f"Books by {author}:")
-    for book in books:
-        print(f"{book['title']} ({book['id']})")
-    print()
-
-    # Call the search_books_by_title() function and print the results
-    title = input("Enter a book title: ")
-    print("Searching on website...")
-    results = search_books_by_title(title)
-    print(f"Search results for '{title}':")
-    print(results)
-    print() """ """
-
-    # Call the get_book_info() function and print the results. 
-    book_id = input("Enter a book ID: ")
-    print("Searching on website...")
-    info = get_book_info(book_id)
-    print(info)
-    print()
-
-    # Call the author_network() function
-    author = input("Enter an author's name to see their co-author network: ")
-    print("Searching on website...")
-    author_network(author)
-
- """
